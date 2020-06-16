da6b9205c98a27836199d406f83420f237205f06
H SH <hmnsmd@gmail.com>
2020-06-16 17:42:42 -0400

    added functionality to copy pass buton and favicon

--- script.js
+++ script.js
@@ -1,119 +1,98 @@
 
 var generateButton = document.getElementById('generate');
 var copyPass = document.getElementById('copy');
-// var lengthInput = document.getElementById('pwLength');
 
-    var makeSpecial = document.getElementById("specialChar");
-    var makeNum = document.getElementById( "numerical");
-    var makeLow = document.getElementById("Lowercase");
-    var makeCaps = document.getElementById("Uppercase");
-    
-
+var makeSpecial = document.getElementById("specialChar");
+var makeNum = document.getElementById( "numerical");
+var makeLow = document.getElementById("Lowercase");
+var makeCaps = document.getElementById("Uppercase");
     
 
+
+// slider
+var slider = document.getElementById("myRange");
+var output = document.getElementById("pwLength");
+output.innerHTML = slider.value + ' characters'; 
+
+slider.oninput = function() {
+output.innerHTML = this.value + ' characters';
+}
+// 
     
-    var slider = document.getElementById("myRange");
-    var output = document.getElementById("pwLength");
-    if (slider.oninput = false){output.innerHTML = slider.value.hide()} else {
-    output.innerHTML = slider.value + ' characters';}
     
+// Function to create and trim password to user selected length   
+function aPassword() {
+    var allChar = '';
+    var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var lower = 'abcdefghijklmnopqrstuvwxyz';
+    var numbers = '123456789012345678901234567890';
+    var specials =  "!\"#$%&\'()*+,-.\/:;<=>?@[\\]^_`{|}~";
+        
+        
+    if(makeCaps.checked){
+    allChar+=(upper);
+    }else{}
+        
+    if(makeNum.checked){
+    allChar+=(numbers);
+    }else{}
+        
+        
+    if(makeLow.checked){
+    allChar+=(lower);
+    } else{}
+        
+    if(makeSpecial.checked){
+    allChar+=(specials);
+    } else{}
+
+        
+    var passwordOutput = '';
+    for ( var i = 0; i < allChar.length; i++ ) {
+    passwordOutput += allChar.charAt(Math.ceil(Math.random() * allChar.length));
+    var trimmedPass = passwordOutput.substr(0, (slider.value))
+    }           
+    document.getElementById('password').innerHTML = trimmedPass;
+    }          
     
-    
-    slider.oninput = function() {
-      output.innerHTML = this.value + ' characters';
+            //    
+              
+   
+
+
+// copy the password 
+function copyThePass() {
+    var copyText = document.getElementById("password");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    alert("Password Copied");
     }
-    
-    
-    
-    function aPassword() {
-        var allChar = '';
-        var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        var lower = 'abcdefghijklmnopqrstuvwxyz';
-        var numbers = '123456789012345678901234567890';
-        var specials =  "!\"#$%&\'()*+,-.\/:;<=>?@[\\]^_`{|}~";
-        
-        
-        if(makeCaps.checked){
-        allChar+=(upper);
-        }else{}
-        
-        if(makeNum.checked){
-        allChar+=(numbers);
-        }else{}
-        
-        
-        if(makeLow.checked){
-        allChar+=(lower);
-        } else{}
-        
-        if(makeSpecial.checked){
-        allChar+=(specials);
-        } else{}
-        
-        // alert(allChar);
 
-        
-                var passwordOutput = '';
-                for ( var i = 0; i < allChar.length; i++ ) {
-                passwordOutput += allChar.charAt(Math.ceil(Math.random() * allChar.length));
-                var trimmedPass = passwordOutput.substr(0, (lengthInput))
-
-            }
-                // alert(passwordOutput)
-                // passwordOutput = string.substring(0, 12);
-                
-                document.getElementById('password').innerHTML = trimmedPass;
-                
-    
-            }    
-
-        
-   
+copyPass.addEventListener('click', function() {
+    return copyThePass()
+    })
+// 
 
 
 
 
-   
-   
-//    generate Button
-     generateButton.addEventListener('click', function() {
+
+            //    generate Button
+generateButton.addEventListener('click', function() {
     
     if (makeCaps.checked || makeLow.checked || makeNum.checked ||makeSpecial.checked) {
-        // alert("checked");
         checked = true;
     } else {
         alert("Select at least one character type.");
         checked = false;
     }
 
-    // var passLength  = lengthInput.value;
-    // if (lengthInput !== undefined  && passLength >= 8 && passLength <=28 ) {
-    //     // alert(passLength);
-    //     lengthEntered = true;
-    // } else {
-    //     alert("Please enter appropriate password length") 
-    //     lengthEntered = false;
-    // }
-    
-   
-    
-    // }
-
-
-   
-   
-   
-   
-    if (checked === true && lengthEntered === true){
+    if (checked === true){
         return aPassword();
     }
-      
-
 })
+// 
 
 
-
-
-
-    
 
